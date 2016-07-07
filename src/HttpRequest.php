95ab75f6c839e16e3fc9fbde4c846264972d8ed1
joecollier <joseph.collier@dm-companies.com>
2016-07-07 16:24:53 -0400

    Update HttpRequest.php

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -25,74 +25,107 @@ class HttpRequest
 
     function __construct($url)
     {
-        if (!function_exists('curl_init')) {
-            echo "Function curl_init, used by HttpRequest does not exist.\n";
-        }
+        // if (!function_exists('curl_init')) {
+        //     echo "Function curl_init, used by HttpRequest does not exist.\n";
+        // }
+        // $this->url = $url;
+        // $this->c = curl_init($this->url);
+        
         $this->url = $url;
-        $this->c = curl_init($this->url);
+        $this->client = new Client(['verify' => false]);
     }
 
     function send($data = null, $filepath = null)
     {
-        if (count($this->headers) > 0) {
-            curl_setopt($this->c, CURLOPT_HEADER, false);
-            curl_setopt($this->c, CURLOPT_HTTPHEADER, $this->headers);
+        // if (count($this->headers) > 0) {
+        //     curl_setopt($this->c, CURLOPT_HEADER, false);
+        //     curl_setopt($this->c, CURLOPT_HTTPHEADER, $this->headers);
+        // }
+
+        // curl_setopt($this->c, CURLOPT_RETURNTRANSFER, true);
+
+        // if ($this->useSsl) {
+        //     curl_setopt($this->c, CURLOPT_SSL_VERIFYPEER, 0);
+        //     curl_setopt($this->c, CURLOPT_SSL_VERIFYHOST, 0);
+        // }
+
+        // if ($this->preventCaching) {
+        //     curl_setopt($this->c, CURLOPT_FORBID_REUSE, true);
+        //     curl_setopt($this->c, CURLOPT_FRESH_CONNECT, true);
+        // }
+
+        // if ($this->uploadMode) {
+        //     //curl_setopt($this->c, CURLOPT_URL, $filepath);
+        //     //curl_setopt($this->c, CURLOPT_UPLOAD, true);
+        //     curl_setopt($this->c, CURLOPT_POST, true);
+        //     $fp = fopen($filepath, 'r');
+        //     curl_setopt($this->c, CURLOPT_INFILE, $fp);
+        //     curl_setopt($this->c, CURLOPT_INFILESIZE, filesize($filepath));
+        // }
+
+        // switch (strtoupper($this->method)) {
+        //     case 'POST':
+        //         curl_setopt($this->c, CURLOPT_POST, true);
+        //         if ($data != null)
+        //             curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
+        //         break;
+
+        //     case 'HEAD':
+        //         curl_setopt($this->c, CURLOPT_NOBODY, true);
+        //         break;
+
+        //     case 'DELETE':
+        //         curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "DELETE");
+        //         break;
+
+        //     case 'PUT':
+        //         curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "PUT");
+        //         if ($data != null)
+        //             curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
+        //         break;
+        // }
+
+        try {
+            switch (strtoupper($this->method)) {
+                case 'POST':
+                    $response = $this->client->request('POST', $this->url, ['json' => $data]);
+                    
+                    if ($data != null) {
+                        // curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
+                    }
+                    break;
+    
+                case 'HEAD':
+                    // curl_setopt($this->c, CURLOPT_NOBODY, true);
+                    break;
+    
+                case 'DELETE':
+                    // curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "DELETE");
+                    break;
+    
+                case 'PUT':
+                    // curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "PUT");
+                    if ($data != null) {
+                        curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
+                    }
+                    
+                    break;
+            }
+        } catch (\GuzzleHttp\Exception\ServerException $e) {
+            $response = $e->getResponse();
         }
 
-        curl_setopt($this->c, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($this->c, CURLOPT_VERBOSE, $this->verbose);
 
-        if ($this->useSsl) {
-            curl_setopt($this->c, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($this->c, CURLOPT_SSL_VERIFYHOST, 0);
-        }
+        // if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
+        //     curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, true);
+        //     $output = curl_exec($this->c);
+        // } else {
+        //     curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, false);
+        //     $output = $this->curlExec($this->c);
+        // }
 
-        if ($this->preventCaching) {
-            curl_setopt($this->c, CURLOPT_FORBID_REUSE, true);
-            curl_setopt($this->c, CURLOPT_FRESH_CONNECT, true);
-        }
-
-        if ($this->uploadMode) {
-            //curl_setopt($this->c, CURLOPT_URL, $filepath);
-            //curl_setopt($this->c, CURLOPT_UPLOAD, true);
-            curl_setopt($this->c, CURLOPT_POST, true);
-            $fp = fopen($filepath, 'r');
-            curl_setopt($this->c, CURLOPT_INFILE, $fp);
-            curl_setopt($this->c, CURLOPT_INFILESIZE, filesize($filepath));
-        }
-
-        switch (strtoupper($this->method)) {
-            case 'POST':
-                curl_setopt($this->c, CURLOPT_POST, true);
-                if ($data != null)
-                    curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
-                break;
-
-            case 'HEAD':
-                curl_setopt($this->c, CURLOPT_NOBODY, true);
-                break;
-
-            case 'DELETE':
-                curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "DELETE");
-                break;
-
-            case 'PUT':
-                curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "PUT");
-                if ($data != null)
-                    curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
-                break;
-        }
-
-        curl_setopt($this->c, CURLOPT_VERBOSE, $this->verbose);
-
-        if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
-            curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, true);
-            $output = curl_exec($this->c);
-        } else {
-            curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, false);
-            $output = $this->curlExec($this->c);
-        }
-
-        return $output;
+        return $response;
     }
 
     function curlExec($ch)
